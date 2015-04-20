394110b4623a273068ff3a173629d686d42ce5e5
StephHippo <stephaniehippo@gmail.com>
2015-04-20 11:56:08 -0400

    Merging clinical data frames.

---
+++ project.r
@@ -0,0 +1,17 @@
+#  Read in the Data
+setwd("/Users/stephaniehippo/Documents/EECS459")
+path <- paste(getwd(), "/project_data", sep="")
+clinical_path = paste(path,"/Clinical",sep="")
+protein_path = paste(path,"/protein_path",sep="")
+methylation_path = paste(path,"/DNA_Methylation", sep="")
+
+#  Clean the data
+ 
+#  Run Feature Selection
+#    LASSO
+
+#  Generate Model
+ 
+
+#  Validate Model
+#    K-Fold Cross-Validation?
