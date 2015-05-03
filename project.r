5675b425c959e4c737e20e2d8707211cce375ca9
StephHippo <stephaniehippo@gmail.com>
2015-05-03 00:00:45 -0400

    Organizing potential dataframe.

--- project.r
+++ project.r
@@ -1,17 +1,49 @@
-#  Read in the Data
-setwd("/Users/stephaniehippo/Documents/EECS459")
-path <- paste(getwd(), "/project_data", sep="")
-clinical_path = paste(path,"/Clinical",sep="")
-protein_path = paste(path,"/protein_path",sep="")
-methylation_path = paste(path,"/DNA_Methylation", sep="")
+library(glmnet)
+library(lars)
 
-#  Clean the data
- 
-#  Run Feature Selection
-#    LASSO
+clinical_expr_integration <- read.csv("~/Documents/EECS459/bioinformatics459/clinical_expr_integration.csv", header=TRUE)
+clin_expr_data_time <- read.csv("~/Documents/EECS459/bioinformatics459/clin_expr_data_time.csv", header=TRUE)
 
-#  Generate Model
- 
+x <- clinical_expr_integration
+y <- clin_expr_data_time$time
 
-#  Validate Model
-#    K-Fold Cross-Validation?
+// Delete useless columns
+x$bcr_patient_uuid <- NULL
+x$bcr_patient_barcode <- NULL
+x$patient_consent_status <- NULL
+x$bcr_drug_uuid <- NULL
+x$bcr_drug_barcode <- NULL
+x$form_completion_date <- NULL
+x$prospective_collection <- NULL
+x$retrospective_collection <- NULL
+x$vital_status <- NULL // Ignore outcome column
+x$initial_pathologic_dx_year <- NULL
+x$days_to_initial_pathologic_diagnosis <- NULL //All 0's
+x$informed_consent_verified <- NULL
+x$patient_id <- NULL
+x$icd_o_3_histology <- NULL
+x$icd_o_3_site <- NULL
+x$tumor_tissue_site <- NULL
+
+// # Impute data or group lasso?
+// gender
+// history other malignancy
+// ajcc_tumor_pathologic_pt
+// ajcc_nodes_pathologic_pn
+// ajcc_metastasis_pathologic_pm
+// er_status_by_ihc
+// pr_status_by_ihc
+// histological_type
+// tissue_source_site
+            
+
+
+# Imputate string columns?
+
+# Try using lars
+summary.lars(data.matrix(x), as.numeric(d_or_a), trace = TRUE)
+
+
+
+# Try using glmnet
+mod <- cv.glmnet(as.matrix(x), y, alpha = 1)
